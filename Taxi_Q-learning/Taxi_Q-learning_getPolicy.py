14d9cbdc2a6f7f0a41cfa09b2bb8442db1172d31
freeEmperor <164176555+freeEmperor@users.noreply.github.com>
2024-09-28 15:07:31 +0800

    Add files via upload

--- Taxi_Q-learning/Taxi_Q-learning_getPolicy.py
+++ Taxi_Q-learning/Taxi_Q-learning_getPolicy.py
@@ -3,22 +3,22 @@ import math
 import numpy as np
 import pandas as pd
 
-q_table = np.zeros( (500,6) )
+q_table = np.zeros( (500, 6) )
 round = 100000
 discount_rate = 0.9
 gamma = 0.1
-epsilon = 0.1
-
+epsilon = 0.6
+state_value_history = np.zeros( (5000, 500) )
 
 def get_rand_ac(obs):
     a = np.random.rand()
-    if a < 6 * epsilon:
+    if a < epsilon:
         return env.action_space.sample()
     return np.argmax(q_table[obs])
 
 if __name__ == '__main__':
     env = gym.make('Taxi-v3', render_mode='ansi')
-    round = 100001
+    round = 100000
     for rd in range(0,round):
         obs, info = env.reset()
         ter = False
@@ -28,6 +28,11 @@ if __name__ == '__main__':
             q_table[obs][ac] = (1 - gamma) * q_table[obs][ac] + gamma * (re + discount_rate * np.max(q_table[obs_new]))
             obs = obs_new
 
+        if rd % 20 == 0:
+            state_value_history[rd//20] = np.max(q_table, axis=1)
+
     Q = pd.DataFrame(q_table)
     Q.to_csv('q_table',index=None)
+    state_value_history_final = pd.DataFrame(state_value_history)
+    state_value_history_final.to_csv('State_value', index=None)
 
